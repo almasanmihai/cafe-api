0e211018e36183aa3a8f7761eae903091faa801e
almasanmihai <almasanmihai@yahoo.com>
2022-07-28 10:01:27 +0300

    reformat code

--- main.py
+++ main.py
@@ -23,8 +23,10 @@ class Cafe(db.Model):
     has_sockets = db.Column(db.Boolean, nullable=False)
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
+
     def to_dict(self):
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 
 @app.route("/")
 def home():
@@ -38,17 +40,17 @@ def random():
     random_cafe = choice(all_cafes)
 
     return jsonify(name=random_cafe.name, id=random_cafe.id, map_url=random_cafe.map_url,
-                       location=random_cafe.location, seats=random_cafe.seats, has_toilet=random_cafe.has_toilet,
-                       has_wifi=random_cafe.has_wifi, has_sockets=random_cafe.has_sockets,
-                       can_take_calls=random_cafe.can_take_calls,
-                       coffee_price=random_cafe.coffee_price)
+                   location=random_cafe.location, seats=random_cafe.seats, has_toilet=random_cafe.has_toilet,
+                   has_wifi=random_cafe.has_wifi, has_sockets=random_cafe.has_sockets,
+                   can_take_calls=random_cafe.can_take_calls,
+                   coffee_price=random_cafe.coffee_price)
 
 
 @app.route("/all")
 def all():
     cafes = db.session.query(Cafe).all()
-    # This uses a List Comprehension but you could also split it into 3 lines.
     return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
+
 
 @app.route("/search")
 def search():
@@ -80,8 +82,6 @@ def add():
     return jsonify(response={"success": "Successfully added the new cafe."})
 
 
-
-
 ## HTTP PUT/PATCH - Update Record
 @app.route("/update-price/<cafe_id>", methods=['PATCH'])
 def patch(cafe_id):
@@ -101,7 +101,7 @@ def delete(cafe_id):
     cafe_to_delete = Cafe.query.get(cafe_id)
     api = request.args.get('key')
     if api == "secret":
-        if cafe_to_delete :
+        if cafe_to_delete:
             db.session.delete(cafe_to_delete)
             db.session.commit()
             return jsonify(response={"success": "Successfully deleted cafe"}), 200
@@ -109,6 +109,7 @@ def delete(cafe_id):
             return jsonify(response={"error": "Not found in database"}), 400
     else:
         return jsonify(response={"error": "Sorry, that's not allowed. Make sure you have the correct api_key"}), 403
+
 
 if __name__ == '__main__':
     app.run(debug=True)
